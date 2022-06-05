834e659972d55997264994072601f41d1a9a7e6a
zhu8 <zhu8@msn.com>
2022-06-05 16:06:21 +0800

    change random images

---
+++ static/assets/js/random-images.js
@@ -0,0 +1,34 @@
+(function() {
+    "use strict";
+    
+    var Images = [
+        '/images/sites/random/dora-fly.png',
+        '/images/sites/random/dora-cast-min.png',
+        '/images/sites/random/dora-allsix.png',
+        '/images/sites/random/dora-family-fly.png',
+        '/images/sites/random/dora-spec.png',
+        '/images/sites/random/dora-cute.png',
+        '/images/sites/random/dora-door.png',
+        '/images/sites/random/dora-draw.png',
+        '/images/sites/random/dora-flying.png',
+        '/images/sites/random/dora-friends.png',
+        '/images/sites/random/dora-logo-all.png',
+        '/images/sites/random/dora-stand.png',
+        '/images/sites/random/dora-time.png',
+        '/images/sites/random/dora-two.png',
+        '/images/sites/random/dora-sailing.png',
+        '/images/sites/random/dora-mantel.png',
+        '/images/sites/random/dora-family.png'
+    ];
+
+    function getRandomImage(images) {
+        var num = Math.floor(Math.random() * images.length),
+            pic = images[num],
+            imgString = '<img src="' + pic + '" id="dora-three" alt="Doraemon-Stand By Me!" title="Doraemon-Stand By Me!" />';
+
+        return imgString;
+    }
+
+    $("#random-images").append(getRandomImage(Images));
+
+})();
